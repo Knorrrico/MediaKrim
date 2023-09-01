d4301922378ff2369bbeb79be640a6135ecb33ad
Knorrrico <rico.knrr@gmail.com>
2023-09-01 18:12:38 +0200

    done

--- 01_preprocessing.R
+++ 01_preprocessing.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 
 #Einlesen Rohdaten
-submissions <- read.csv2("data/submissions.csv", 
+submissions <- read.csv2("data_raw/submissions.csv", 
                            sep = ",", colClasses=c(NA), header = FALSE)
 
 #Flair Kategorien 
@@ -14,7 +14,7 @@ submissions_crime <- submissions |>
   filter(V6 == "category crime")
 
 submissions_ref <- submissions |> 
-  filter(V6 == c("category newsde", "category newseu", "category newswo"))
+  filter(V6 == c("category newsde", "category newseu", "category newswo", "category politics", "category society"))
 
 #ID Liste zum filtern von Kommentaren
 list <- submissions_crime$V7
@@ -25,10 +25,10 @@ lapply(list_ref, write, "list_ref.txt", append=TRUE)
 
 #Einsetzen der Listen in Python Script
 
-comments_crime <- read.csv2("data/comments_crime.csv", 
-                                  sep = ",", colClasses=c(NA), header = FALSE)
-comments_ref <- read.csv2("data/comments_ref.csv", 
+comments_crime <- read.csv2("data_raw/comments_crime.csv", 
                             sep = ",", colClasses=c(NA), header = FALSE)
+comments_ref <- read.csv2("data_raw/comments_ref.csv", 
+                          sep = ",", colClasses=c(NA), header = FALSE)
 
 #Umbenennen der Variablen
 names(comments_crime)
@@ -43,7 +43,14 @@ submissions_ref <- submissions_ref |>
 submissions_crime <- submissions_crime |> 
   rename(score = V1, date = V2, title = V3, user = V4, link = V5, flair = V6, id = V7, body = V8)
 
+names(comments_crime)
+
 #Entfernen von NA
+comments_crime |> 
+  group_by(body) |> 
+  summarise(count = n()) |> 
+  arrange(desc(count))
+
 submissions_crime <- submissions_crime |> 
   filter(body != "[deleted]") |> 
   filter(body != "[removed]") |> 
@@ -51,7 +58,7 @@ submissions_crime <- submissions_crime |>
   mutate(title = tolower(title)) |> 
   mutate(body = tolower(body))
 
-submissions_ref <- submissions_ref |> 
+submissions_ref <- submissions_ref |>
   filter(body != "[deleted]") |> 
   filter(body != "[removed]") |> 
   filter(body != "") |> 
@@ -70,7 +77,23 @@ comments_ref <- comments_ref |>
   filter(body != "") |> 
   mutate(body = tolower(body))
 
-write.csv2(submissions_crime, "data/submissions_crime.csv", row.names = FALSE)fff
+comments_crime |> 
+  group_by(body) |> 
+  summarise(count = n()) |> 
+  arrange(desc(count))
+
+#ID anpassen 
+comments_crime$id <- comments_crime$id |> 
+  str_replace_all("t3_", "")
+
+comments_ref$id <- comments_ref$id |> 
+  str_replace_all("t3_", "")
+
+#Nur OC Beiträge. Von Nutzern formuliert.
+selftext_crime <- submissions_crime |> 
+  filter(!str_detect(body, "^http"))
+
+write.csv2(submissions_crime, "data/submissions_crime.csv", row.names = FALSE)
 write.csv2(submissions_ref, "data/submissions_ref.csv", row.names = FALSE)
 write.csv2(comments_crime, "data/comments_crime.csv", row.names = FALSE)
 write.csv2(comments_ref, "data/comments_ref.csv", row.names = FALSE)
