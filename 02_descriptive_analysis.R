18d627429efd2c8de18df8f3091e46f80ed3c98a
Knorrrico <rico.knrr@gmail.com>
2023-09-01 18:11:54 +0200

    weekly frequency

--- 02_descriptive_analysis.R
+++ 02_descriptive_analysis.R
@@ -1,15 +1,14 @@
 #descriptive analysis / exploration
 library(tidyverse)
-library(quanteda)
-library(quanteda.textstats)
 library(lubridate)
 
-comments_crime <- read.csv2("Data/comments_crime.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
-comments_ref <- read.csv2("Data/comments_ref.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
+comments_crime <- read.csv2("data/comments_crime.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
+comments_ref <- read.csv2("data/comments_ref.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
 
-submissions_crime <- read.csv2("Data/submissions_crime.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
-submissions_ref <- read.csv2("Data/submissions_ref.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
+submissions_crime <- read.csv2("data/submissions_crime.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
+submissions_ref <- read.csv2("data/submissions_ref.csv", colClasses=c(NA), header = TRUE, stringsAsFactors = FALSE)
 
+#
 #Posthäufigkeit von Nutzern zählen
 comments_crime |> 
   group_by(user) |> 
@@ -21,14 +20,12 @@ submissions_crime |>
   summarize(count = n()) |> 
   arrange(desc(count))
 
-#Populärste Beiträge nach Kommentarhäufigkeit
-
+#Populärste Beiträge nach Kommentarhäufigkeit (NAs sind beiträge die vorher gefilter wurden)
 comments_crime <- comments_crime |> 
   left_join(submissions_crime |> select(id, title), by = "id")
 
 comments_ref <- comments_ref|> 
   left_join(submissions_ref |> select(id, title), by = "id")
-
 
 comments_crime |> 
   group_by(title) |> 
@@ -37,20 +34,21 @@ comments_crime |>
   arrange(desc(count)) |> 
   print()
 
-#Populärste Beiträge nach Score
+comments_crime <- comments_crime |> 
+  filter(!is.na(title))
 
+comments_ref <- comments_ref |> 
+  filter(!is.na(title))
+
+#Populärste Beiträge nach Score (datensatz mehrmals aktualisiert)
 submissions_crime |> 
   arrange(desc(score)) |> 
   select(id, score, title) |> 
   head(10) |> 
   print()
 
-#Nur OC Beiträge. Von Nutzern formuliert.(Für den Blogpost nur noch Selftext)
-selftext_crime <- submissions_crime |> 
-  filter(!str_detect(body, "^http"))
 
 #Beide Dataframes mergen
-
 selected_crime_submissions <- submissions_crime |> 
   select(id, body, flair)
 
@@ -59,8 +57,7 @@ merged_crime <- merge(selected_crime_submissions, comments_crime, by.x = "id", b
 merged_crime <- merged_crime |> 
   rename(content = body.x, body = body.y)
 
-#reference
-
+#Reference
 selected_ref_submissions <- submissions_ref |> 
   select(id, body, flair)
 
@@ -70,7 +67,6 @@ merged_ref <- merged_ref |>
   rename(content = body.x, body = body.y)
 
 #Start- und Enddatum
-
 min(merged_crime$date)
 max(merged_crime$date)
 
@@ -78,7 +74,6 @@ min(selftext_crime$date)
 max(selftext_crime$date)
 
 #Kommentaraufkommen nach Zeit
-
 merged_crime <- merged_crime |> 
   mutate(date = as.Date(date, format = "%Y-%m-%d"))
 
@@ -86,63 +81,48 @@ merged_crime |>
   group_by(date) |> 
   summarise(count = n())
 
-comments_per_day <- merged_crime |> 
-  mutate(month = floor_date(date, unit = "month")) |> 
-  group_by(month) |> 
+comments_per_week <- merged_crime |> 
+  mutate(week = floor_date(date, unit = "week")) |> 
+  group_by(week) |> 
   summarise(id = n())
 
-ggplot(comments_per_day, aes(x = month, y = id)) +
-  geom_line(color = "blue") +
-  geom_point(size = 3, color = "red") +
+ggplot(comments_per_week, aes(x = week, y = id)) +
+  geom_col() +
+  geom_smooth() +
   labs(
     title = "Comment Distribution Over Time",
     x = "Date",
     y = "Number of Comments"
   ) +
-  theme_minimal()
+  theme_light() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  scale_x_date(date_breaks = "1 year", date_labels = "%Y")
 
-#Text Preprocessing
+# Wochen mit stärksten Ausprägungen
+comments_per_week <- comments_per_week|> 
+  arrange(desc(id))
 
-replacements <- c("ä" = "ae", "ö" = "oe", "ü" = "ue", "ß" = "ss")
+start_date <- as.Date("2020-06-14")
+end_date <- as.Date("2020-06-28")
 
-replace_umlauts <- function(text) {
-  for (char in names(replacements)) {
-    text <- stringi::stri_replace_all_fixed(text, char, replacements[char], case_insensitive = TRUE)
-  }
-  return(text)
-}
+most_comments_week <- comments_crime |> 
+  filter(date >= start_date & date <= end_date)
 
-merged_crime <- merged_crime %>%
-  mutate(body = replace_umlauts(body),
-         user = replace_umlauts(user),
-         title = replace_umlauts(title)
-  )
+summary_week <- most_comments_week |> 
+  group_by(id) |> 
+  summarise(n_comments = n()) |> 
+  arrange(desc(n_comments))
 
-merged_ref <- merged_ref %>%
-  mutate(body = replace_umlauts(body),
-         user = replace_umlauts(user),
-         title = replace_umlauts(title)
-  )
+top_3 <- as.list(head(summary_week$id, 3))
 
-corpus_crime <- corpus(merged_crime, text_field = "body")
-corpus_crime <- corpus_group(corpus_crime, groups = id)
+summary_week
+# Title der populärsten Beiträge
 
-df_combined <- rbind(merged_crime, merged_ref)
-corpus_combined <- corpus(df_combined, text_field = "body")
-#corpus_combined <- corpus_group(corpus_combined, groups = flair)
+submissions_crime |> 
+  filter(id %in% top_3) |> 
+  select(title)
 
-tokens_combined <- tokens(corpus_combined,
-                          remove_punct = TRUE,
-                          remove_symbols = TRUE,
-                          remove_numbers = TRUE,
-                          remove_url = TRUE,
-                          remove_separators = TRUE)
+#Speicher zusammengefassten Datensatz aus Submission und Comment
+write.csv2(merged_crime, "data/merged_crime.csv", row.names = FALSE)
+write.csv2(merged_ref, "data/merged_ref.csv", row.names = FALSE)
 
-tokens_combined <- tokens_remove(tokens_combined, pattern = stopwords("de"))
-
-
-
-tokens_combined <- tokens_group(tokens_combined, groups = flair)
-
-dfm_combined <- dfm(tokens_combined)
-keyness <- textstat_keyness(dfm_combined)
